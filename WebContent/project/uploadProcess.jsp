d52163846387cce8e790c56b43d4837672053b1c
kimkuan <zxd46@kpu.ac.kr>
2019-11-19 19:15:50 +0900

    upload project

--- WebContent/project/uploadProcess.jsp
+++ WebContent/project/uploadProcess.jsp
@@ -11,34 +11,59 @@
 <title>Insert title here</title>
 </head>
 <body>
-	<jsp:setProperty property="*" name="bean"/>
+
 	<%
-		int user_no, en_no;
-	    String id = (String)session.getAttribute("idKey"); // 현재 로그인한 회원의 id 가져오기
-	 
-	    user_no = account.getUserNo(id);   
-		Date date = new Date();
+		request.setCharacterEncoding("utf-8");
 		
-	
-		bean.setWrite_time("현재 시간");
-		
-		
-		
-		// projectUpload 함수를 실행했을 때의 결과값을 확인
-		
-		boolean result = upload.projectUpload(bean);
+		int user_no, en_no = -1;
+		String write_time;
 		String msg = "등록을 실패하였습니다.";
+   		String id = (String)session.getAttribute("idKey"); // 현재 로그인한 회원의 id 가져오기
+	
+   		System.out.println(id);
+   		
+   		if(id == null){
+			msg = "로그인이 필요합니다.";
+			response.sendRedirect("/guguya/user/login.jsp");
+		}	
+
+	    user_no = account.getUserNo(id); // id에 따른 user_no 받아오기
+		en_no = upload.getEnno(user_no);
+	    System.out.println("en : " + en_no);
+	    Date date = new Date();
+		write_time = date.toString();
+	%>
+		<jsp:setProperty name="bean" property="proj_name" />
+		<jsp:setProperty name="bean" property="proj_desc"/>
+	<% 
+		if(en_no > 0){ // 반환 값이 제대로 들어옴
+			bean.setEn_no(en_no); // 현재 user의 en_no 값을 넣기
+			bean.setWrite_time(write_time);
+
+			System.out.println(bean.getProj_name());
+			System.out.println(bean.getProj_desc());
+			System.out.println(bean.getEn_no());
+			System.out.println(bean.getWrite_time());
+		}
+		else{ // 반환 값이 없으면
+			System.out.println("권한이 없습니다.");
+			// response.sendRedirect("/guguya/user/login.jsp");
+		}
+
+		// projectUpload 함수를 실행했을 때의 결과값을 확인	
+		boolean result = upload.projectUpload(bean);
+	
 		
 		//성공 여부에 따라 메세지 출력
 		if(result){
-			msg="등록하였습니다.";
-			
+			msg="등록하였습니다.";	
+			response.sendRedirect("/guguya/project/projectUpload.jsp");
 		}
 	%>
 <script>
 	//메세지 출력
 	alert("<%=msg%>");
 </script>
-	<% response.sendRedirect("login.jsp"); %>
+
 </body>
 </html>
--- src/guguya/projectMigrate.java
+++ src/guguya/projectMigrate.java
@@ -2,13 +2,16 @@ package guguya;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Date;
 import guguya.dbCon;
 import guguya.projectBean;
 
 
+
 public class projectMigrate {
-	Connection con = null;
-	dbCon dbcon = new dbCon();
+
+	static dbCon dbcon = new dbCon();
+	static Connection con = dbcon.getConnection();
 	
 	public boolean projectUpload(projectBean bean) throws ClassNotFoundException, SQLException {
 		PreparedStatement pstmt = null;
@@ -16,12 +19,16 @@ public class projectMigrate {
 		String sql = null;
 		boolean flag = false;
 		try {
-			con = (Connection)dbcon;
-			sql = "insert project(proj_name, proj_desc, write_time)"+ "values(?,?,?)";
+			// con = dbcon.getConnection();
+			sql = "insert project(proj_name, proj_desc, en_no, write_time) values(?,?,?,?)";
+			
+			Date date = new Date();
+			
 			pstmt = con.prepareStatement(sql);
 			pstmt.setString(1, bean.getProj_name());
 			pstmt.setString(2, bean.getProj_desc());
-			pstmt.setString(3, bean.getWrite_time());
+			pstmt.setInt(3, bean.getEn_no());
+			pstmt.setString(4, date.toString());
 			if(pstmt.executeUpdate()==1) flag=true;
 		}catch(Exception e) {
 			e.printStackTrace();
@@ -30,22 +37,22 @@ public class projectMigrate {
 		return flag;
 	}
 	
-	//회원 id로 en_no가져오기 (보류)
-	public int getEn_No(String id) throws SQLException {
+	//user_no으로 en_no가져오기 (보류)
+	public int getEnno(int user_no) throws SQLException {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		String sql=null;
-		int user_no=1;
+		int en_no = -1;
 		try {
-			con = (Connection)dbcon;
-			sql = "select * from user where id=?";
+			// con = dbcon.getConnection();
+			sql = "select en_no from enterprise where user_no=?";
 			pstmt = con.prepareStatement(sql);
-			pstmt.setString(1,id);
+			pstmt.setInt(1,user_no);
 			rs = pstmt.executeQuery();
 			while(rs.next()) {
-				user_no=rs.getInt("user_no");
+				en_no = rs.getInt("en_no");
 			}
-			return user_no;
+			return en_no;
 		}finally {
 			
 		}
